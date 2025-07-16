db04bc832aa44e271aeed1c969e346731280ead2
Ash <ash@drashsmith.com>
2025-07-16 12:26:39 +0100

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
 from setuptools import setup
-setup(name='hedgehog', version='3.0.0', packages=['hedgehog'], entry_points={'console_scripts': ['hedgehog = hedgehog.__main__:main']})
+import os
+base_path = os.path.dirname(os.path.abspath(__file__))
+req_file = os.path.join(base_path, 'requirements.txt')
+with open(req_file, 'r') as fp:
+	req = fp.read().splitlines()
+setup(name='hedgehog', version='3.0.0', packages=['hedgehog'], entry_points={'console_scripts': ['hedgehog = hedgehog.__main__:main']}, install_requires=req)
